8e37ac0c71b92a9b7cd4d49a202c27b14ec57080
User3 <User3@LAPTOP-QS1FM7NH>
2021-07-07 08:29:44 -0500

    JB 1

--- Jul21_Java/src/com/ss/jb/one/Numbers.java
+++ Jul21_Java/src/com/ss/jb/one/Numbers.java
@@ -5,60 +5,53 @@ import java.util.Random;
 
 /**
  * Java Basics 1 - Assignment 2
- * User has 5 attempts to guess a number within +/-10 of a randomly generated number.
+ * 
+ * User has 5 attempts to guess within +/-10 of a randomly generated number.
  * 
  * @author Tim Ning
  *
  */
-public class Numbers 
-{
+public class Numbers {
 	/**
 	 * Start the program.
+	 * 
 	 * @param args
 	 */
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		Random rand = new Random();
 		boolean success = false;
-		
-		//generate a random number from 1-100 inclusive
+
+		// generate a random number from 1-100 inclusive
 		int num = rand.nextInt(100) + 1;
-		
-		//initialize outside of the guess range
+
+		// initialize outside of the guess range
 		int guess = Integer.MIN_VALUE;
 
 		System.out.println("Guess a number from 1-100:");
 
-		//user attempts 5 guesses
-		for(int i=4; i>=0; i--)
-		{
-			//standard input via console
-			try 
-			{
+		// user attempts 5 guesses
+		for (int i = 4; i >= 0; i--) {
+			// standard input via console
+			try {
 				guess = Integer.parseInt(br.readLine());
-		    }
-			catch (Exception ex) 
-			{
-				//input was not an integer
-		    	System.out.println("Invalid input!");
-		    }
-			
-			//determine whether guess is within 10 of the random number
-			if(Math.abs(num - guess) <= 10)
-			{
+			} catch (Exception ex) {
+				// input was not an integer
+				System.out.println("Invalid input!");
+			}
+
+			// determine whether guess is within 10 of the random number
+			if (Math.abs(num - guess) <= 10) {
 				success = true;
 				break;
-			}
-			else if(i > 0)
-			{
+			} else if (i > 0) {
 				System.out.println("Try again. " + i + " attempt" + (i == 1 ? "" : "s") + " remaining:");
 			}
 		}
-		
-		//print the outcome
+
+		// print the outcome
 		System.out.print(success ? "Success!" : "Sorry!");
 		System.out.println(" The number was " + num + ".");
 	}
-	
+
 }
